41a6f34a2e6cae8877762d66dc78aaff9220a82f
Jelatinone <115746975+Jelatinone@users.noreply.github.com>
2022-10-19 20:09:36 -0500

    Add files via upload

---
+++ FortuneWheelRunner.java
@@ -0,0 +1,20 @@
+//Project : Wheel of Fortune
+//Date : 10/19/2022
+//Name : Cody Washington
+
+//Packages
+import java.lang.*;
+import java.util.Scanner;
+
+//Runner Class
+public class FortuneWheelRunner
+{
+    //Runner
+    public static void main(String[] args)
+    {
+        //Instantiation
+        Scanner input = new Scanner(System.in);
+        FWGraphics ConsoleHandler = new FWGraphics();
+        FortuneWheel GameHandler = new FortuneWheel();
+    }
+}
