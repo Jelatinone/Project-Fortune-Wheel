1ad0eaeed80cd6f1b2a7528622328bc3f74766be
Jelatinone <115746975+Jelatinone@users.noreply.github.com>
2022-11-03 19:46:18 -0500

    Final Commit
    Various Logic Changes Runner Side, Graphics Now function completely, Case Sensitivity changes, Credits.

--- FortuneWheelRunner.java
+++ FortuneWheelRunner.java
@@ -1,20 +1,91 @@
-//Project : Wheel of Fortune
-//Date : 10/19/2022
-//Name : Cody Washington
+// Class - AP Computer Science A
+// Date - 10/12/2022
+// Name - Cody Washington
 
 //Packages
 import java.lang.*;
 import java.util.Scanner;
-
-//Runner Class
+//Main Runner Class
 public class FortuneWheelRunner
 {
-    //Runner
     public static void main(String[] args)
     {
-        //Instantiation
+        //Credit
+        System.out.println("[Cody Washington | 10/12/2022 | Wheel of Fortune]");
+        //Spacing
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.println();
+        }
+        //Setup
         Scanner input = new Scanner(System.in);
-        FWGraphics ConsoleHandler = new FWGraphics();
-        FortuneWheel GameHandler = new FortuneWheel();
+        //Input the Phrase
+        System.out.print("Your Phrase :: ");
+        String phrase = input.nextLine();
+        //Clear Console
+        System.out.print("\033[H\033[2J");  
+        //Create a new game
+        FortuneWheel game = new FortuneWheel(phrase.toLowerCase());
+        //Gameplay Main-Loop
+        while(game.getGameStatus() == false)
+        {
+            //Prinout Information
+            game.graphics();
+            //Input Guess Forward to Guess Method
+            System.out.print("Enter a Guess :: ");
+            String response = input.nextLine();
+            
+            //Print a message
+            switch (game.Guess(response))
+            {
+                case 0:
+                    System.out.println("[You've already guessed that.]");
+                    break;
+                case 1:
+                    System.out.println("[Your guess was in the Phrase.]");
+                    break;
+                case 2:
+                    System.out.println("[Your guess wasn't in the Phrase.]");
+                    break;
+                case 3:
+                    System.out.println("[Your guess wasn't the Phrase.]");
+                    break;
+                case 4:
+                    System.out.println("[Your guess was the Phrase.]");
+                    //Update to display the Phrase
+                    game.graphics();
+                    System.out.println("[Press Anything to Continue.]");
+                    input.nextLine();
+                    //Ask if Player wants to Continue
+                    game.continueplaying();
+                    break;
+                case 5:
+                    System.out.println("[You guessed everything.]");
+                    //Update to display the Phrase
+                    game.graphics();
+                    System.out.println("[Press Anything to Continue.]");
+                    input.nextLine();
+                    //Ask if Player wants to Continue
+                    game.continueplaying();
+                    break;
+                case 6:
+                    System.out.println("[Invalid Guess.]");
+                    //Ask if Player wants to Continue
+                    game.continueplaying();
+                    break;
+            }
+            //Check if Replay
+            if(!(game.getGuessCount() >= 0))
+            {
+                game.continueplaying();
+            }
+        }
+        //Credits
+        System.out.println("_______________________");
+        System.out.println("|                     |");
+        System.out.println("| Thanks for Playing! |");
+        System.out.println("|  - Cody Washington  |");
+        System.out.println("|                     |");
+        System.out.println("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");        
     }
 }
